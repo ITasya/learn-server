851abd187aae6ba2a22d7adca80d212da6120da5
ITasya <nas.popova030003@gmail.com>
2026-05-28 06:04:51 +0500

    add env

--- Client/src/Helpers/axiosinstance.js
+++ Client/src/Helpers/axiosinstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
 
 const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL=BASE_URL;
-axiosInstance.defaults.withCredentials=true;
+axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
